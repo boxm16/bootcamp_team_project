70854cdba053eafea587d1415102ca26fc6f9c6d
sitmalidis <sitmalidi@yahoo.com>
2018-11-14 18:49:02 +0200

    trial

--- src/java/Controller/UserController.java
+++ src/java/Controller/UserController.java
@@ -5,10 +5,39 @@
  */
 package Controller;
 
+import Dao.UserDao;
+import Model.Sports;
+import Model.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  *
  * @author Herc
  */
+@Controller
+
 public class UserController {
+     @Autowired
+    private UserDao userDao;
+     
+     @RequestMapping(value = "/adduser.htm", method = RequestMethod.GET)
+    public String adduser(ModelMap model) {
+       Users user=new Users();
+       user.setFirstname("v");
+       user.setLastname("dv");
+       user.setPassword("1");
+       user.setUsername("bbb");
+       Sports sport=new Sports();
+       sport.setSportsId(1);
+       sport.setSports("football");
+       user.setSports(sport);
+         userDao.insert(user);
+        return "index";
+
+    }
     
 }
--- src/java/Dao/UserDao.java
+++ src/java/Dao/UserDao.java
@@ -1,10 +1,26 @@
-
 package Dao;
+
+import Model.Users;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Herc
  */
+@Repository
 public class UserDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional
+    public void insert(Users user) {
+
+        em.persist(user);
+
+    }
 
 }
