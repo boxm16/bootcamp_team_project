3fccb7d21b180619948770e68230ba9c1d2bee26
sitmalidis <sitmalidi@yahoo.com>
2018-12-20 16:24:20 +0200

    FilterFixed

--- src/java/Filter/AuthenticationFilter.java
+++ src/java/Filter/AuthenticationFilter.java
@@ -17,7 +17,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 /**
  *
  * @author Michail Sitmalidis
@@ -26,7 +25,7 @@ public class AuthenticationFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig); 
+        Filter.super.init(filterConfig);
     }
 
     @Override
@@ -40,16 +39,29 @@ public class AuthenticationFilter implements Filter {
         String logingURI = request.getContextPath() + "/loginFormHandling.htm";
         String registerURI = request.getContextPath() + "/goToRegisterForm.htm";
         String registeringURI = request.getContextPath() + "/registerFormHandling.htm";
+        String loginKarouselPictureURI = request.getContextPath() + "/resources/banner.jpg";
+        String loginKarouselPictureURI1 = request.getContextPath() + "/resources/soccer.jpg";
+        String loginKarouselPictureURI2 = request.getContextPath() + "/resources/logo.jpg";
+        String loginKarouselPictureURI3 = request.getContextPath() + "/resources/tennis.jpg";
+        String loginKarouselPictureURI4 = request.getContextPath() + "/resources/giannis.jpg";
+        String javascriptFile = request.getContextPath() + "/resources/newjavascript.js";
+        String cssFile = request.getContextPath() + "/resources/newcss2.css";
 
         boolean loggedIn = session != null && session.getAttribute("user") != null;
         boolean logingRequest = request.getRequestURI().equals(logingURI);
         boolean loginRequest = request.getRequestURI().equals(loginURI);
         boolean registerRequest = request.getRequestURI().equals(registerURI);
         boolean registeringRequest = request.getRequestURI().equals(registeringURI);
-       
+        boolean loginKarouselPictureURIRequest = request.getRequestURI().equals(loginKarouselPictureURI);
+        boolean loginKarouselPictureURI1Request = request.getRequestURI().equals(loginKarouselPictureURI1);
+        boolean loginKarouselPictureURI2Request = request.getRequestURI().equals(loginKarouselPictureURI2);
+        boolean loginKarouselPictureURI3Request = request.getRequestURI().equals(loginKarouselPictureURI3);
+        boolean loginKarouselPictureURI4Request = request.getRequestURI().equals(loginKarouselPictureURI4);
+        boolean javascriptFileRequest = request.getRequestURI().equals(javascriptFile);
+        boolean cssFileRequest = request.getRequestURI().equals(cssFile);
         System.out.println(new Date() + "URI:" + request.getRequestURI());
 
-        if (loggedIn || loginRequest ||logingRequest|| registerRequest||registeringRequest) {
+        if (loggedIn || loginRequest || logingRequest || registerRequest || registeringRequest || loginKarouselPictureURIRequest || loginKarouselPictureURI1Request || loginKarouselPictureURI2Request || loginKarouselPictureURI3Request || loginKarouselPictureURI4Request || javascriptFileRequest || cssFileRequest) {
             chain.doFilter(request, response);
         } else {
             response.sendRedirect(loginURI);
